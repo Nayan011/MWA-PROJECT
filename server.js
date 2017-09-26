7d0fc441236d2c2231d9ba58a45e48e9a19df1af
Nuruzzaman Bepari <30918905+Nayan011@users.noreply.github.com>
2017-09-26 14:04:21 -0500

    Update server.js

--- server.js
+++ server.js
@@ -25,19 +25,54 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
  mongoose.connect(config.database);
 
- const serviceProvider=require('./controllers/ServiceProviderController');
- const customerService=require('./controllers/CustomerController');
  
-app.get('/createService',function(req,res){
+ var checkAuth = function (req, res, next) {
+    console.log("Checking authentication");
+  
+    if (typeof req.cookies.nToken === 'undefined' || req.cookies.nToken === null) {
+      req.provider = null;
+    } else {
+      var token = req.cookies.nToken;
+      var decodedToken = jwt.decode(token, { complete: true }) || {};
+      let data = decodedToken.payload;
+      req.providerEmail=data.email;
+      //console.log("email from Auth"+req.provider.email);
+      console.log("from d"+req.cookies.nToken);
+    }
+  
+    next()
+  };
+  
+  app.use(checkAuth);
+  const serviceProvider=require('./controllers/ServiceProviderController');
+  const serviceProviderAuth=require('./controllers/ServiceProviderAuth');
+  const customerService=require('./controllers/CustomerController');
+ 
+app.get('/createServiceProvider',function(req,res){
 
-    res.render('./addService.ejs');
+    res.render('./createServiceProvider.ejs');
 });
-app.post('/createService',serviceProvider.addServiceProvider);
+app.post('/createServiceProvider',serviceProvider.addServiceProvider);
 app.get('/createCustomer',function(req,res){
     
         res.render('./addCustomer.ejs');
     });
-    app.post('/createCustomer',customerService.addCustomer);
+app.post('/createCustomer',customerService.addCustomer);
+
+app.get('/addService',(req,res)=>{
+    res.render("./providerProfile.ejs");
+});
+app.post('/addService',serviceProvider.addService);
+app.get('/updateService',serviceProvider.updateService);
+app.get('/singleService/:id',serviceProvider.getSingleService);
+app.post('/updateService/:id',serviceProvider.updateService);
+app.get('/providerLogin',function(req,res){
+
+    res.render('./providerLogin.ejs');
+});
+app.post('/providerLogin',serviceProviderAuth.ServiceProviderLogin);
+//app.get('/providerLogout',serviceProviderAuth.providerLogout);
+
 
  // START THE SERVER
  // ===============================
