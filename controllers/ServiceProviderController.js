04ab3f5c4729f18e91dc93aa637d59f92b1a749d
Nuruzzaman Bepari <30918905+Nayan011@users.noreply.github.com>
2017-09-26 10:36:03 -0500

    Update ServiceProviderController.js

--- controllers/ServiceProviderController.js
+++ controllers/ServiceProviderController.js
@@ -1,5 +1,5 @@
 var ServiceProvider = require('../models/ServiceProvider');
-//var Product = require('../models/Product.server.model');
+var Customer = require('../models/Customer');
 
 module.exports={
 
@@ -9,6 +9,8 @@ module.exports={
      let serviceProvider=new ServiceProvider({
 
         name:req.body.name,
+        email:req.body.email,
+        password:req.body.password,
         licenceNo:req.body.licenceNo,
         accountNo:req.body.accountNo,
         bankName:req.body.bankName,
@@ -22,13 +24,80 @@ module.exports={
     }
      
      console.log(data);
-     res.redirect('/createService');
+     res.redirect('/providerLogin');
      });
 
 
-   }
+   },
+   addService:function(req,res,next){
+       
+       Customer.findOne({"_id":"59c92126b46e8e29f8b33648"},(err,cus)=>{
+        var data={
+            
+            fromLoc:req.body.fromLoc,
+            toLoc:req.body.toLoc,
+            pickDate:req.body.pickDate,
+            dropDate:req.body.dropDate,
+            amountReceived:req.body.amountReceived,
+            status:req.body.status,
+            customer:cus
+        };
+    ServiceProvider.update({"email":req.providerEmail },{$push:{"services":data}},(err,da)=>{
+ 
+     console.log(da);
+     res.end();
+ 
+ 
+    });
 
-     
+       });
+
+   },
+
+   updateService:function(req,res){
+
+    ServiceProvider.update({"services._id":req.params.id},
+    {$set:{"services.$.status":req.body.status,"services.$.amountReceived":req.body.amountReceived}},(err,da)=>{
+
+        console.log(da);
+        res.end();
+    });
+
+   },
+   getSingleService:function(req,res){
+    ServiceProvider.findOne({"services._id":req.params.id},(err,data)=>{
+
+        var service={};
+        service=data.services.find(x=>req.params.id==x._id);
+        console.log(service);
+        res.render('./singleService.ejs',{service:service});
+        
+    });
+    
+   }
+   /*,
+   findProviderServiceByEmail:function(req,res){
+
+    ServiceProvider.findOne({"email":req.body.email},(err,provider)=>{
+        console.log(provider);
+
+        if(err){
+            res.redirect('/providerLogin');
+        }
+        if(provider){
+            req.session.providerEmail=provider.email;
+            res.render('./providerProfile.ejs',{provider:provider});
+
+        }
+        
+    });
+   },
+   providerLogout:function(req,res){
+
+    req.session.providerEmail="";
+    res.redirect('/providerLogin');
+   }
+     */
 
 }
 
