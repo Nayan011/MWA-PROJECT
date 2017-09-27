d4f48053bdba908b425fbac1dc2681c98bd271f6
Jayakumar Lavudi <jayakumar.lavudi@gmail.com>
2017-09-27 10:54:33 -0500

    Add files via upload

--- controllers/CustomerController.js
+++ controllers/CustomerController.js
@@ -10,6 +10,7 @@ module.exports={
 
         name:req.body.name,
         email:req.body.email,
+        password:req.body.password,
         mobileNo:req.body.mobileNo,
     
 
@@ -26,7 +27,54 @@ module.exports={
      });
 
 
-   }
+   },
+
+   addTransaction:function(req,res,next){
+    
+    ServiceProvider.findOne({"_id":"59c92785af5b400a38514492"},(err,provider)=>{
+     var data={
+         
+         fromLoc:req.body.fromLoc,
+         toLoc:req.body.toLoc,
+         pickDate:req.body.pickDate,
+         dropDate:req.body.dropDate,
+         budget:req.body.budget,
+         status:req.body.status,
+         provider:provider
+     };
+     Customer.update({"email":req.customerEmail },{$push:{"transactions":data}},(err,da)=>{
+
+  console.log(da);
+  res.end();
+
+
+ });
+
+    });
+
+},
+
+updateTransaction:function(req,res){
+
+    Customer.update({"transactions._id":req.params.id},
+ {$set:{"transactions.$.status":req.body.status,"transactions.$.budget":req.body.Budget}},(err,detail)=>{
+
+     console.log(detail);
+     res.end();
+ });
+
+},
+getSingleTransaction:function(req,res){
+    Customer.findOne({"transactions._id":req.params.id},(err,data)=>{
+
+     var custDetails={};
+     custDetails=data.transactions.find(x=>req.params.id==x._id);
+     console.log(custDetails);
+     res.render('./SingleTransaction.ejs',{tran:custDetails});
+     
+ });
+ 
+}
 
      
 
